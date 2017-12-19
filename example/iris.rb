052848bedda90d80268bdf37b7df0341639ec447
Kouhei Sutou <kou@clear-code.com>
2017-12-19 21:11:01 +0900

    Add an example to load iris data

---
+++ example/iris.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+
+require "datasets"
+
+iris = Datasets::Iris.new
+iris.each do |record|
+  p [
+     record.sepal_length,
+     record.sepal_width,
+     record.petal_length,
+     record.petal_width,
+     record.class,
+  ]
+  # [5.1, 3.5, 1.4, 0.2, "Iris-setosa"]
+  # [7.0, 3.2, 4.7, 1.4, "Iris-versicolor"]
+end

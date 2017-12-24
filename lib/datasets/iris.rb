c72e776337b295a998666609590ab423028f49b0
Kouhei Sutou <kou@clear-code.com>
2017-12-25 00:43:28 +0900

    iris: download data automatically

--- lib/datasets/iris.rb
+++ lib/datasets/iris.rb
@@ -1,4 +1,7 @@
-require 'csv'
+require "csv"
+require "fileutils"
+require "open-uri"
+require "pathname"
 
 module Datasets
   class Iris
@@ -8,18 +11,53 @@ module Datasets
                         :petal_width,
                         :class)
 
-    def initialize
-      path = File.expand_path('../../data/iris.csv', File.dirname(__FILE__))
-      @csv = CSV.open(path, converters: [:numeric])
-    end
-
     def each
       return to_enum(__method__) unless block_given?
 
-      @csv.each do |row|
-        next if row[0].nil?
-        record = Record.new(*row)
-        yield(record)
+      open_csv do |csv|
+        csv.each do |row|
+          next if row[0].nil?
+          record = Record.new(*row)
+          yield(record)
+        end
+      end
+    end
+
+    private
+    def cache_dir_path
+      case RUBY_PLATFORM
+      when /mswin/, /mingw/
+        base_dir = ENV["LOCALAPPDATA"] || "~/AppData"
+      when /darwin/
+        base_dir = "~/Library/Caches"
+      else
+        base_dir = ENV["XDG_CACHE_HOME"] || "~/.cache"
+      end
+      Pathname(base_dir).expand_path + "red-datasets" + "iris"
+    end
+
+    def data_path
+      @data_path ||= cache_dir_path + "iris.csv"
+    end
+
+    def open_csv
+      download unless data_path.exist?
+      CSV.open(data_path, converters: [:numeric]) do |csv|
+        yield(csv)
+      end
+    end
+
+    def download
+      data_path.parent.mkpath
+      begin
+        open("https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data") do |input|
+          data_path.open("wb") do |output|
+            IO.copy_stream(input, output)
+          end
+        end
+      rescue
+        FileUtils.rm_f(data_path)
+        raise
       end
     end
   end

57ffc1508f095b65e05363c50489c4ac0afc9a2d
hatappi <hatappi@hatappi.me>
2018-04-26 22:39:09 +0900

    add cifar-test

---
+++ test/test-cifar.rb
@@ -0,0 +1,85 @@
+class CifarTest < Test::Unit::TestCase
+  sub_test_case("cifar-10") do
+    sub_test_case("train") do
+      def setup
+        @dataset = Datasets::Cifar.new(class_num: 10, set_type: :train)
+      end
+
+      test("#each") do
+        records = @dataset.to_a
+        assert_equal([
+                        50000,
+                        3072,
+                        Integer,
+                      ],
+                      [
+                        records.size,
+                        records[0].data.size,
+                        records[0].label.class
+                      ]) 
+      end
+    end
+
+    sub_test_case("test") do
+      def setup
+        @dataset = Datasets::Cifar.new(class_num: 10, set_type: :test)
+      end
+
+      test("#each") do
+        records = @dataset.to_a
+        assert_equal([
+                        10000,
+                        3072,
+                        Integer,
+                      ],
+                      [
+                        records.size,
+                        records[0].data.size,
+                        records[0].label.class
+                      ]) 
+      end
+    end
+  end
+  
+  sub_test_case("cifar-100") do
+    sub_test_case("train") do
+      def setup
+        @dataset = Datasets::Cifar.new(class_num: 100, set_type: :train)
+      end
+
+      test("#each") do
+        records = @dataset.to_a
+        assert_equal([
+                        50000,
+                        3072,
+                        Integer,
+                      ],
+                      [
+                        records.size,
+                        records[0].data.size,
+                        records[0].label.class
+                      ]) 
+      end
+    end
+
+    sub_test_case("test") do
+      def setup
+        @dataset = Datasets::Cifar.new(class_num: 100, set_type: :train)
+      end
+
+      test("#each") do
+        records = @dataset.to_a
+        assert_equal([
+                        10000,
+                        3072,
+                        Integer,
+                      ],
+                      [
+                        records.size,
+                        records[0].data.size,
+                        records[0].label.class
+                      ]) 
+      end
+    end
+  end
+end

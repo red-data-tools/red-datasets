0b1324b96fee0a9a991a67cb41f327faae245884
Kouhei Sutou <kou@clear-code.com>
2018-06-06 15:01:00 +0900

    Table: add fetch_values

--- test/test-table.rb
+++ test/test-table.rb
@@ -3,6 +3,41 @@ class TableTest < Test::Unit::TestCase
     @table = Datasets::Iris.new.to_table
   end
 
+  test("#[]") do
+    assert_equal([1.4, 1.4, 1.3, 1.5, 1.4],
+                 @table[:petal_length].first(5))
+  end
+
+  sub_test_case("#fetch_values") do
+    test("found") do
+      values = @table.fetch_values(:petal_length, :petal_width)
+      assert_equal([
+                     [1.4, 1.4, 1.3, 1.5, 1.4],
+                     [0.2, 0.2, 0.2, 0.2, 0.2],
+                   ],
+                   values.collect {|v| v.first(5)})
+    end
+
+    sub_test_case("not found") do
+      test("with block") do
+        values = @table.fetch_values(:petal_length, :unknown) do |key|
+          [key] * 5
+        end
+        assert_equal([
+                       [1.4, 1.4, 1.3, 1.5, 1.4],
+                       [:unknown] * 5,
+                     ],
+                     values.collect {|v| v.first(5)})
+      end
+
+      test("without block") do
+        assert_raise(KeyError) do
+          @table.fetch_values(:unknown)
+        end
+      end
+    end
+  end
+
   test("#to_h") do
     shorten_hash = {}
     @table.to_h.each do |name, values|

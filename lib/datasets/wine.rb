328d6bc13e76574df927e24473c2dcb7104bf1da
鈴木竜太 <django.contrib.auth.models@gmail.com>
2018-10-13 12:23:32 +0900

    add wine dataset class

---
+++ lib/datasets/wine.rb
@@ -0,0 +1,64 @@
+require 'csv'
+
+require_relative 'dataset'
+
+module Datasets
+  class Wine < Dataset
+    Record = Struct.new(:wine_class,
+                        :alcohol,
+                        :malic_acid,
+                        :ash,
+                        :alcalinity_of_ash,
+                        :magnesium,
+                        :total_phenols,
+                        :flavanoids,
+                        :nonflavanoid_phenols,
+                        :proanthocyanins,
+                        :color_intensity,
+                        :hue,
+                        :od280_od315_of_diluted_wines,
+                        :proline)
+
+    def initialize
+      super
+      @metadata.id = 'wine'
+      @metadata.name = 'Wine'
+      @metadata.url = 'http://archive.ics.uci.edu/ml/datasets/wine'
+      @metadata.description = -> { read_names }
+    end
+
+    def each
+      return to_enum(__method__) unless block_given?
+
+      open_data do |csv|
+        csv.each do |row|
+          next if row[0].nil?
+          record = Record.new(*row)
+          yield(record)
+        end
+      end
+    end
+
+    private
+
+    def read_names
+      names_path = cache_dir_path + 'wine.names'
+      unless names_path.exist?
+        names_url = 'http://archive.ics.uci.edu/ml/machine-learning-databases/wine/wine.names'
+        download(names_path, names_url)
+      end
+      names_path.read
+    end
+
+    def open_data
+      data_path = cache_dir_path + 'wine.data'
+      unless data_path.exist?
+        data_url = 'http://archive.ics.uci.edu/ml/machine-learning-databases/wine/wine.data'
+        download(data_path, data_url)
+      end
+      CSV.open(data_path, converters: %i[numeric]) do |csv|
+        yield(csv)
+      end
+    end
+  end
+end

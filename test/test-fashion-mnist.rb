42242a457fb4004f509e66647d967552c8ebbbd7
Kouhei Sutou <kou@clear-code.com>
2018-10-15 14:05:24 +0900

    test: use real dataset

--- test/test-fashion-mnist.rb
+++ test/test-fashion-mnist.rb
@@ -2,99 +2,106 @@ class FashionMNISTTest < Test::Unit::TestCase
   include Helper::Sandbox
 
   sub_test_case("Normal") do
-    def setup_data
-      setup_sandbox
-
-      def @dataset.cache_dir_path
-        @cache_dir_path
-      end
-
-      def @dataset.cache_dir_path=(path)
-        @cache_dir_path = path
-      end
-      @dataset.cache_dir_path = @tmp_dir
-
-      def @dataset.download(output_path, url)
-        image_magic_number = 2051
-        label_magic_number = 2049
-        n_image, image_size_x, image_size_y, label = 10, 28, 28, 1
-
-        Zlib::GzipWriter.open(output_path) do |gz|
-          if output_path.basename.to_s.include?("-images-")
-            image_data = ([image_magic_number, n_image]).pack('N2') +
-                         ([image_size_x,image_size_y]).pack('N2') +
-                         ([0] * image_size_x * image_size_y).pack("C*") * n_image
-            gz.puts(image_data)
-          else
-            label_data = ([label_magic_number, n_image]).pack('N2') +
-                         ([label] * n_image).pack("C*")
-            gz.puts(label_data)
-          end
-        end
-      end
-    end
-
-    def teardown
-      teardown_sandbox
-    end
-
     sub_test_case("train") do
       def setup
         @dataset = Datasets::FashionMNIST.new(type: :train)
-        setup_data()
       end
 
       test("#each") do
-        raw_dataset = @dataset.collect do |record|
-          {
-            :label => record.label,
-            :pixels => record.pixels
-          }
-        end
-
+        records = @dataset.each.to_a
         assert_equal([
-                       {
-                         :label => 1,
-                         :pixels => [0] * 28 * 28
-                       }
-                     ] * 10,
-                     raw_dataset)
+                       60000,
+                       [
+                         5,
+                         784,
+                         [0, 0, 0, 49, 238, 253, 253, 253, 253, 253],
+                         [0, 0, 0, 0, 0, 81, 240, 253, 253, 119],
+                       ],
+                       [8,
+                         784,
+                         [0, 0, 0, 0, 0, 0, 0, 0, 0, 62],
+                         [0, 0, 190, 196, 14, 2, 97, 254, 252, 146],
+                       ],
+                     ],
+                     [
+                       records.size,
+                       [
+                         records[0].label,
+                         records[0].pixels.size,
+                         records[0].pixels[200, 10],
+                         records[0].pixels[400, 10],
+                       ],
+                       [
+                         records[-1].label,
+                         records[-1].pixels.size,
+                         records[-1].pixels[200, 10],
+                         records[-1].pixels[400, 10],
+                       ],
+                     ])
       end
 
       test("#to_table") do
         table_data = @dataset.to_table
-        assert_equal([[0] * 28 * 28] * 10,
-                     table_data[:pixels])
+        assert_equal([
+                       [0, 0, 0, 49, 238, 253, 253, 253, 253, 253],
+                       [0, 0, 0, 0, 0, 0, 0, 0, 0, 62],
+                     ],
+                     [
+                       table_data[:pixels][0][200, 10],
+                       table_data[:pixels][-1][200, 10],
+                     ])
       end
     end
 
     sub_test_case("test") do
       def setup
         @dataset = Datasets::FashionMNIST.new(type: :test)
-        setup_data()
       end
 
       test("#each") do
-        raw_dataset = @dataset.collect do |record|
-          {
-            :label => record.label,
-            :pixels => record.pixels
-          }
-        end
-
+        records = @dataset.each.to_a
         assert_equal([
-                       {
-                         :label => 1,
-                         :pixels => [0] * 28 * 28
-                       }
-                     ] * 10,
-                     raw_dataset)
+                       10000,
+                       [
+                         9,
+                         784,
+                         [1, 0, 0, 0, 98, 136, 110, 109, 110, 162],
+                         [172, 161, 189, 62, 0, 68, 94, 90, 111, 114],
+                       ],
+                       [
+                         5,
+                         784,
+                         [45, 45, 69, 128, 100, 120, 132, 123, 135, 171],
+                         [63, 74, 72, 0, 1, 0, 0, 0, 4, 85],
+                       ],
+                     ],
+                     [
+                       records.size,
+                       [
+                         records[0].label,
+                         records[0].pixels.size,
+                         records[0].pixels[400, 10],
+                         records[0].pixels[500, 10],
+                       ],
+                       [
+                         records[-1].label,
+                         records[-1].pixels.size,
+                         records[-1].pixels[400, 10],
+                         records[-1].pixels[500, 10],
+                       ],
+                     ])
       end
 
       test("#to_table") do
         table_data = @dataset.to_table
-        assert_equal([[0] * 28 * 28] * 10,
-                     table_data[:pixels])
+        assert_equal([
+                       [1, 0, 0, 0, 98, 136, 110, 109, 110, 162],
+                       [45, 45, 69, 128, 100, 120, 132, 123, 135, 171],
+                     ],
+                     [
+                       table_data[:pixels][0][400, 10],
+                       table_data[:pixels][-1][400, 10],
+                     ])
       end
     end
   end
--- test/test-mnist.rb
+++ test/test-mnist.rb
@@ -2,99 +2,106 @@ class MNISTTest < Test::Unit::TestCase
   include Helper::Sandbox
 
   sub_test_case("Normal") do
-    def setup_data
-      setup_sandbox
-
-      def @dataset.cache_dir_path
-        @cache_dir_path
-      end
-
-      def @dataset.cache_dir_path=(path)
-        @cache_dir_path = path
-      end
-      @dataset.cache_dir_path = @tmp_dir
-
-      def @dataset.download(output_path, url)
-        image_magic_number = 2051
-        label_magic_number = 2049
-        n_image, image_size_x, image_size_y, label = 10, 28, 28, 1
-
-        Zlib::GzipWriter.open(output_path) do |gz|
-          if output_path.basename.to_s.include?("-images-")
-            image_data = ([image_magic_number, n_image]).pack('N2') +
-                         ([image_size_x,image_size_y]).pack('N2') +
-                         ([0] * image_size_x * image_size_y).pack("C*") * n_image
-            gz.puts(image_data)
-          else
-            label_data = ([label_magic_number, n_image]).pack('N2') +
-                         ([label] * n_image).pack("C*")
-            gz.puts(label_data)
-          end
-        end
-      end
-    end
-
-    def teardown
-      teardown_sandbox
-    end
-
     sub_test_case("train") do
       def setup
         @dataset = Datasets::MNIST.new(type: :train)
-        setup_data()
       end
 
       test("#each") do
-        raw_dataset = @dataset.collect do |record|
-          {
-            :label => record.label,
-            :pixels => record.pixels
-          }
-        end
-
+        records = @dataset.each.to_a
         assert_equal([
-                       {
-                         :label => 1,
-                         :pixels => [0] * 28 * 28
-                       }
-                     ] * 10,
-                     raw_dataset)
+                       60000,
+                       [
+                         5,
+                         784,
+                         [0, 0, 0, 49, 238, 253, 253, 253, 253, 253],
+                         [0, 0, 0, 0, 0, 81, 240, 253, 253, 119],
+                       ],
+                       [8,
+                         784,
+                         [0, 0, 0, 0, 0, 0, 0, 0, 0, 62],
+                         [0, 0, 190, 196, 14, 2, 97, 254, 252, 146],
+                       ],
+                     ],
+                     [
+                       records.size,
+                       [
+                         records[0].label,
+                         records[0].pixels.size,
+                         records[0].pixels[200, 10],
+                         records[0].pixels[400, 10],
+                       ],
+                       [
+                         records[-1].label,
+                         records[-1].pixels.size,
+                         records[-1].pixels[200, 10],
+                         records[-1].pixels[400, 10],
+                       ],
+                     ])
       end
 
       test("#to_table") do
         table_data = @dataset.to_table
-        assert_equal([[0] * 28 * 28] * 10,
-                     table_data[:pixels])
+        assert_equal([
+                       [0, 0, 0, 49, 238, 253, 253, 253, 253, 253],
+                       [0, 0, 0, 0, 0, 0, 0, 0, 0, 62],
+                     ],
+                     [
+                       table_data[:pixels][0][200, 10],
+                       table_data[:pixels][-1][200, 10],
+                     ])
       end
     end
 
     sub_test_case("test") do
       def setup
         @dataset = Datasets::MNIST.new(type: :test)
-        setup_data()
       end
 
       test("#each") do
-        raw_dataset = @dataset.collect do |record|
-          {
-            :label => record.label,
-            :pixels => record.pixels
-          }
-        end
-
+        records = @dataset.each.to_a
         assert_equal([
-                       {
-                         :label => 1,
-                         :pixels => [0] * 28 * 28
-                       }
-                     ] * 10,
-                     raw_dataset)
+                       10000,
+                       [
+                         7,
+                         784,
+                         [0, 0, 84, 185, 159, 151, 60, 36, 0, 0],
+                         [0, 0, 0, 0, 0, 0, 0, 0, 59, 249],
+                       ],
+                       [
+                         6,
+                         784,
+                         [0, 0, 0, 0, 0, 15, 60, 60, 168, 253],
+                         [253, 253, 132, 64, 0, 0, 18, 43, 157, 171],
+                       ],
+                     ],
+                     [
+                       records.size,
+                       [
+                         records[0].label,
+                         records[0].pixels.size,
+                         records[0].pixels[200, 10],
+                         records[0].pixels[400, 10],
+                       ],
+                       [
+                         records[-1].label,
+                         records[-1].pixels.size,
+                         records[-1].pixels[200, 10],
+                         records[-1].pixels[400, 10],
+                       ],
+                     ])
       end
 
       test("#to_table") do
         table_data = @dataset.to_table
-        assert_equal([[0] * 28 * 28] * 10,
-                     table_data[:pixels])
+        assert_equal([
+                       [0, 0, 84, 185, 159, 151, 60, 36, 0, 0],
+                       [0, 0, 0, 0, 0, 15, 60, 60, 168, 253],
+                     ],
+                     [
+                       table_data[:pixels][0][200, 10],
+                       table_data[:pixels][-1][200, 10],
+                     ])
       end
     end
   end

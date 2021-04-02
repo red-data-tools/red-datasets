92876cd4fc8ae6a36c1721151b9f24c33de3f76f
Kenta Murata <mrkn@mrkn.jp>
2021-04-02 16:55:47 +0900

    Rename class: Rdatasets::Master -> RdatasetsList

--- lib/datasets/error.rb
+++ lib/datasets/error.rb
@@ -1,4 +1,7 @@
 module Datasets
   class Error < StandardError
   end
+
+  class FileNotFound < Error
+  end
 end
--- lib/datasets/rdatasets.rb
+++ lib/datasets/rdatasets.rb
@@ -2,90 +2,88 @@ require_relative "dataset"
 require_relative "tar_gz_readable"
 
 module Datasets
-  class Rdatasets < Dataset
-    class FileNotFound < StandardError; end
+  class RdatasetsList < Dataset
+    include TarGzReadable
 
-    class Master < Dataset
-      include TarGzReadable
+    def initialize
+      super
+      @metadata.id = "Rdatasets"
+      @metadata.name = "Rdatasets"
+      @metadata.url = "https://vincentarelbundock.github.io/Rdatasets/"
+      @metadata.licenses = ["GPL-3"]
+      @data_url = "https://github.com/vincentarelbundock/Rdatasets/archive/refs/heads/master.tar.gz"
+      @data_path = cache_dir_path + "rdatasets.tar.gz"
+    end
 
-      def initialize
-        super
-        @metadata.id = "Rdatasets"
-        @metadata.name = "Rdatasets"
-        @metadata.url = "https://vincentarelbundock.github.io/Rdatasets/"
-        @metadata.licenses = ["GPL-3"]
-        @data_url = "https://github.com/vincentarelbundock/Rdatasets/archive/refs/heads/master.tar.gz"
-        @data_path = cache_dir_path + "rdatasets.tar.gz"
+    attr_reader :data_path
+
+    def download(force: false)
+      if force || !@data_path.exist?
+        super(@data_path, @data_url)
       end
+    end
 
-      attr_reader :data_path
-
-      def download(force: false)
-        if force || !@data_path.exist?
-          super(@data_path, @data_url)
-        end
-      end
-
-      def dataset_info(package_name, dataset_name)
-        each_dataset(package_name) do |row|
-          if row["Item"] == dataset_name
-            record = row.to_h
-            record.transform_keys! {|key| key.downcase.to_sym }
-            return record
-          end
-        end
-      end
-
-      def exist?(package_name, dataset_name = nil)
-        if dataset_name.nil?
-          each_dataset(package_name) { return true }
-          return false
-        else
-          each_dataset(package_name).any? {|row| row["Item"] == dataset_name }
-        end
-      end
-
-      def each_dataset(package_name = nil)
-        return to_enum(__method__, package_name) unless block_given?
-
-        download
-        open_dataset_file("datasets.csv") do |entry|
-          csv = CSV.parse(entry.read, col_sep: ",", row_sep: :auto, headers: :first_row, quote_char: '"')
-          csv.each do |row|
-            if package_name.nil? || row["Package"] == package_name
-              yield row
-            end
-          end
-        end
-      rescue FileNotFound
-        raise "Unable to find datasets.csv. " +
-              "Please try Datasets::Rdatasets.update to update the data source."
-      end
-
-      def open_dataset_file(path)
-        open_tar(data_path) do |tar|
-          tar.seek("Rdatasets-master/#{path}") do |entry|
-            return yield(entry)
-          end
-          raise FileNotFound, "File not found in Rdatasets: #{path}"
+    def dataset_info(package_name, dataset_name)
+      each_dataset(package_name) do |row|
+        if row["Item"] == dataset_name
+          record = row.to_h
+          record.transform_keys! {|key| key.downcase.to_sym }
+          return record
         end
       end
     end
 
+    def exist?(package_name, dataset_name = nil)
+      if dataset_name.nil?
+        each_dataset(package_name) { return true }
+        return false
+      else
+        each_dataset(package_name).any? {|row| row["Item"] == dataset_name }
+      end
+    end
+
+    def each_dataset(package_name = nil)
+      return to_enum(__method__, package_name) unless block_given?
+
+      download
+      open_dataset_file("datasets.csv") do |entry|
+        csv = CSV.parse(entry.read, col_sep: ",", row_sep: :auto, headers: :first_row, quote_char: '"')
+        csv.each do |row|
+          if package_name.nil? || row["Package"] == package_name
+            yield row
+          end
+        end
+      end
+    rescue FileNotFound
+      raise "Unable to find datasets.csv. " +
+            "Please try Datasets::Rdatasets.update to update the data source."
+    end
+
+    def open_dataset_file(path)
+      open_tar(data_path) do |tar|
+        tar.seek("Rdatasets-master/#{path}") do |entry|
+          return yield(entry)
+        end
+        raise FileNotFound, "File not found in Rdatasets: #{path}"
+      end
+    end
+  end
+
+  class Rdatasets < Dataset
     def self.update
-      Master.new.download(force: true)
+      RdatasetsList.new.download(force: true)
     end
 
     def self.datasets(package_name = nil)
-      Master.new.each_dataset(package_name).to_a
+      RdatasetsList.new.each_dataset(package_name).to_a
     end
 
     def self.exist?(package_name, dataset_name = nil)
-      Master.new.exist?(package_name, dataset_name)
+      RdatsetsList.new.exist?(package_name, dataset_name)
     end
 
     def initialize(package_name, dataset_name)
-      master = Master.new
+      master = RdatasetsList.new
       unless master.exist?(package_name, dataset_name)
         raise ArgumentError, "Unable to locate dataset #{package_name}/#{dataset_name}"
       end
@@ -109,7 +107,7 @@ module Datasets
       return to_enum(__method__) unless block_given?
 
       csv_name = File.join(@package_name, @dataset_name) + ".csv"
-      Master.new.open_dataset_file("csv/#{csv_name}") do |entry|
+      RdatasetsList.new.open_dataset_file("csv/#{csv_name}") do |entry|
         read_csv_entry(entry.read, &block)
       end
     end

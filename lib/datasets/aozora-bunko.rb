63bcfbddf782d1914d58aef775f08176b6d2dff1
Masayuki Morisaki <masayuki144@gmail.com>
2021-09-23 21:18:29 +0900

    Supporting only extended source file.

--- lib/datasets/aozora-bunko.rb
+++ lib/datasets/aozora-bunko.rb
@@ -3,25 +3,7 @@ require_relative 'dataset'
 module Datasets
   # Dataset for AozoraBunko
   class AozoraBunko < Dataset
-    # 人物ID,著者名,作品ID,作品名,仮名遣い種別,翻訳者名等,入力者名,校正者名,状態,状態の開始日,底本名,出版社名,入力に使用した版,校正に使用した版
     Record = Struct.new(
-      :person_id,
-      :person_name,
-      :title_id,
-      :title,
-      :syllabary_spelling_type, # 仮名遣い種別
-      :translator_name,
-      :registered_person_name,
-      :proofreader_name,
-      :status,
-      :status_specified_date,
-      :original_book_name,
-      :original_book_publisher_name,
-      :used_version_for_registration,
-      :used_version_for_proofreading
-    )
-
-    RecordExtended = Struct.new(
       # 作品ID,作品名,作品名読み,ソート用読み,副題,副題読み,原題,初出,分類番号,文字遣い種別,作品著作権フラグ,公開日,最終更新日,図書カードURL,
       :title_id,
       :title,
@@ -86,13 +68,7 @@ module Datasets
       :html_file_updating_count
     )
 
-    TYPES = %i[normal extended].freeze
-
-    def initialize(type: :normal)
-      unless TYPES.include?(type)
-        raise ArgumentError, "Please specify #{TYPES.map(&:to_s).join(' or ')} for :type. <type: #{type}>"
-      end
-
+    def initialize
       super()
 
       @metadata.id = 'aozora-bunko'
@@ -104,8 +80,6 @@ module Datasets
         on the Internet like a library. The copyrighted works and the works that are said to be
         "free to read" are available after being digitized in text and XHTML (some HTML) formats.
       DESCRIPTION
-
-      @type = type
     end
 
     def each
@@ -115,7 +89,7 @@ module Datasets
         text = csv_file_stream.read.force_encoding(Encoding::UTF_8) # file has Byte Order Mark
 
         CSV.parse(text, headers: true) do |row|
-          yield(record_class.new(*row.fields))
+          yield(Record.new(*row.fields))
         end
       end
     end
@@ -123,18 +97,13 @@ module Datasets
     private
 
     def data_path
-      cache_dir_path + "#{@type}.zip"
+      cache_dir_path + 'list_person_all_extended_utf8.zip'
     end
 
     def download
       return if data_path.exist?
 
-      case @type
-      when :normal
-        data_url = 'https://www.aozora.gr.jp/index_pages/list_person_all_utf8.zip'
-      when :extended
-        data_url = 'https://www.aozora.gr.jp/index_pages/list_person_all_extended_utf8.zip'
-      end
+      data_url = 'https://www.aozora.gr.jp/index_pages/list_person_all_extended_utf8.zip'
       super(data_path, data_url)
     end
 
@@ -147,15 +116,6 @@ module Datasets
 
           entry.get_input_stream(&block)
         end
-      end
-    end
-
-    def record_class
-      case @type
-      when :normal
-        Record
-      when :extended
-        RecordExtended
       end
     end
   end

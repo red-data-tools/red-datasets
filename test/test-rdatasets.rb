a720cf7ee5d7dd54ca0c6aa43b25ab332aa32470
Sutou Kouhei <kou@clear-code.com>
2021-10-29 05:43:34 +0900

    test rdatasets: update expected

--- test/test-rdatasets.rb
+++ test/test-rdatasets.rb
@@ -48,7 +48,7 @@ class RdatasetsTest < Test::Unit::TestCase
       test("without package_name") do
         records = @dataset.each.to_a
         assert_equal([
-                       1738,
+                       1741,
                        {
                          package: "AER",
                          dataset: "Affairs",

f78581ac5a3d8ee5e46ae361911101615cedd5d2
bkmgit <benson_muite@emailplus.org>
2021-11-24 18:42:28 +0300

    get tests to pass

--- lib/datasets/libsvm-dataset-list.rb
+++ lib/datasets/libsvm-dataset-list.rb
@@ -78,6 +78,13 @@ module Datasets
     def open_detail(detail)
       data_path = cache_dir_path + detail
       download(data_path, @metadata.url + detail)
+      # Hack to fix XML error in EUR-Lex section of
+      # https://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/multilabel.html
+      if detail == "multilabel.html"
+        page = data_path.read
+        page.gsub!("'>', '<',", "'&gt', '&lt',")
+        data_path.write(page)
+      end
       data_path.open do |input|
         yield(input)
       end

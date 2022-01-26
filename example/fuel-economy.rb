294ab7d6f26c8f3066ab3c7085213435baf2bb26
Sutou Kouhei <kou@clear-code.com>
2022-01-26 17:04:02 +0900

    Update example

--- example/fuel-economy.rb
+++ example/fuel-economy.rb
@@ -7,12 +7,17 @@ diamonds.each do |record|
   p [
      record.manufacturer,
      record.model,
-     record.trans,
-     record.cty,
-     record.hwy
+     record.displacement,
+     record.year,
+     record.n_cylinders,
+     record.transmission,
+     record.drive_train,
+     record.city_mpg,
+     record.highway_mpg,
+     record.fuel,
+     record.type,
   ]
+  # ["audi", "a4", 1.8, 1999, 4, "auto(l5)", "f", 18, 29, "p", "compact"]
+  # ["audi", "a4", 1.8, 1999, 4, "manual(m5)", "f", 21, 29, "p", "compact"]
+  # ...
 end
-
-#  ["audi", "a4", "auto(l5)", 18, 29]
-#  ["audi", "a4", "manual(m5)", 21, 29]
-

d8420c4fff8a0f0845736242d5ad74c76e754817
Sutou Kouhei <kou@clear-code.com>
2022-01-26 16:34:05 +0900

    Remove needless instance variable

--- lib/datasets/fuel-economy.rb
+++ lib/datasets/fuel-economy.rb
@@ -19,22 +19,20 @@ module Datasets
       @metadata.licenses = ["CC0-1.0"]
       @metadata.url = "https://ggplot2.tidyverse.org/reference/mpg.html"
       @metadata.description = "Fuel economy dataset from ggplot2, originally from https://www.fueleconomy.gov"
-
-      @data_path = cache_dir_path + "mpg.csv"
     end
 
     def each
       return to_enum(__method__) unless block_given?
 
+      data_path = cache_dir_path + "mpg.csv"
       data_url = "https://github.com/tidyverse/ggplot2/raw/main/data-raw/mpg.csv"
-      download(@data_path, data_url)
-      CSV.open(@data_path, headers: :first_row, converters: :all) do |csv|
+      download(data_path, data_url)
+      CSV.open(data_path, headers: :first_row, converters: :all) do |csv|
         csv.each do |row|
           record = Record.new(*row.fields)
           yield record
         end
       end
     end
-
   end
 end

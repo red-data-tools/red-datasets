f0f39f2d60db3a31d556dfafe9cc0b49b19ddcab
Sutou Kouhei <kou@clear-code.com>
2022-05-16 11:49:13 +0900

    Organize test

--- test/test-wikipedia-kyoto-japanese-english.rb
+++ test/test-wikipedia-kyoto-japanese-english.rb
@@ -1,76 +1,158 @@
 class WikipediaKyotoJapaneseEnglishTest < Test::Unit::TestCase
-  def setup
-    @dataset_articles = Datasets::WikipediaKyotoJapaneseEnglish.new(type: :article)
-    @dataset_lexicon = Datasets::WikipediaKyotoJapaneseEnglish.new(type: :lexicon)
-  end
+  sub_test_case("article") do
+    def setup
+      @dataset = Datasets::WikipediaKyotoJapaneseEnglish.new(type: :article)
+    end
 
-  test('article') do
-    first_article = @dataset_articles.each.first
-    assert_equal([
-                    "jawiki-20080607-pages-articles.xml",
-                    "copyright (c) 2010 前田左衛門佐(id:194701), 与左衛門(id:159462), R28Bot(id:177951), 遠藤(id:30275), Dream100(id:41924), 独立行政法人情報通信研究機構．このテキストの利用はCreative Commons Attribution-Share-Alike License 3.0 の条件の下に許諾されます．（この著作権者一覧には、Wikipedia IPユーザーを含んでおりません．この点に関するお問い合わせはこちらまで：kyoto-corpus@khn.nict.go.jp）",
-                    9,
-                    0
-                  ],
-                  [
-                    first_article.to_h[:source],
-                    first_article.to_h[:copyright],
-                    first_article.to_h[:contents].size,
-                    first_article.to_h[:sections].size
-                  ])
-  end
+    def shorten_text(text)
+      max = 20
+      if text.size <= max
+        text
+      else
+        "#{text[0, max]}..."
+      end
+    end
 
-  test('title_and_sentence') do
-    title, sentence = @dataset_articles.each.first.to_h[:contents].first(2)
-    assert_equal([
-                  [
-                    nil,
-                    "三要元佶",
-                    "Genkitsu SANYO"
-                  ],
-                  [
-                    "1",
-                    nil,
-                    "1",
-                    1,
-                    "三要元佶（さんよう げんきつ, 天文 (元号)17年（1548年） - 慶長17年5月20日 (旧暦)（1612年6月19日））は、安土桃山時代から江戸時代初頭にかけて活躍した禅僧である。",
-                    "Genkitsu SANYO (1548 - June 19, 1612) was a Zen priest who was active during the period from the Azuchi-Momoyama period to the early Edo period."]
-                  ],
-                  [
-                   [
-                     title.to_h[:section],
-                     title.to_h[:japanese],
-                     title.to_h[:english]
-                   ],
-                   [
-                     sentence.to_h[:id],
-                     sentence.to_h[:section],
-                     sentence.to_h[:paragraph][:id],
-                     sentence.to_h[:paragraph][:sentences].size,
-                     sentence.to_h[:japanese],
-                     sentence.to_h[:english]
-                    ]
-                  ])
-  end
+    def hashify(record)
+      hash = {class: record.class.name.split("::").last}
+      case record
+      when Datasets::WikipediaKyotoJapaneseEnglish::Title
+        hash[:section] = record.section&.id
+        hash[:japanese] = shorten_text(record.japanese)
+        hash[:english] = shorten_text(record.english)
+      when Datasets::WikipediaKyotoJapaneseEnglish::Sentence
+        hash[:id] = record.id
+        hash[:section] = record.section&.id
+        hash[:paragraph] = record.paragraph&.id
+        hash[:japanese] = shorten_text(record.japanese)
+        hash[:english] = shorten_text(record.english)
+      else
+        record.members.each do |member|
+          value = record[member]
+          case value
+          when Array
+            value = value.collect do |v|
+              hashify(v)
+            end
+          when String
+            value = shorten_text(value)
+          when Struct
+            value = hasify(value)
+          end
+          hash[member] = value
+        end
+      end
+      hash
+    end
 
-  test('lexicon') do
-    records = @dataset_lexicon.each.to_a
-    assert_equal([
-                   51982,
-                   {
-                     :japanese => "102世吉田日厚貫首",
-                     :english => "the 102nd head priest, Nikko TOSHIDA"
+    test("#each") do
+      first_record = @dataset.each.first
+      assert_equal({
+                     class: "Article",
+                     copyright: "copyright (c) 2010 前...",
+                     sections: [],
+                     source: "jawiki-20080607-page...",
+                     contents: [
+                       {
+                         class: "Title",
+                         section: nil,
+                         english: "Genkitsu SANYO",
+                         japanese: "三要元佶",
+                       },
+                       {
+                         class: "Sentence",
+                         id: "1",
+                         section: nil,
+                         paragraph: "1",
+                         english: "Genkitsu SANYO (1548...",
+                         japanese: "三要元佶（さんよう げんきつ, 天文 (...",
+                       },
+                       {
+                         class: "Sentence",
+                         id: "2",
+                         section: nil,
+                         paragraph: "2",
+                         english: "He was originally fr...",
+                         japanese: "肥前国（佐賀県）の出身。",
+                       },
+                       {
+                         class: "Sentence",
+                         id: "3",
+                         section: nil,
+                         paragraph: "2",
+                         english: "His Go (pen name) wa...",
+                         japanese: "号は閑室。",
+                       },
+                       {
+                         class: "Sentence",
+                         id: "4",
+                         section: nil,
+                         paragraph: "2",
+                         english: "He was called Kiccho...",
+                         japanese: "佶長老、閑室和尚と呼ばれた。",
+                       },
+                       {
+                         class: "Sentence",
+                         id: "5",
+                         section: nil,
+                         paragraph: "3",
+                         english: "He went up to the ca...",
+                         japanese: "幼少時に都に上り、岩倉の円通寺 (京都市...",
+                       },
+                       {
+                         class: "Sentence",
+                         id: "6",
+                         section: nil,
+                         paragraph: "4",
+                         english: "After assuming the p...",
+                         japanese: "足利学校の長となるが、関ヶ原の戦いの折に...",
+                       },
+                       {
+                         class: "Sentence",
+                         id: "7",
+                         section: nil,
+                         paragraph: "5",
+                         english: "He assumed the posit...",
+                         japanese: "金地院崇伝と寺社奉行の任に当たり、西笑承...",
+                       },
+                       {
+                         class: "Sentence",
+                         id: "8",
+                         section: nil,
+                         paragraph: "6",
+                         english: "Later, he was invite...",
+                         japanese: "家康によって、伏見区の学校に招かれ、円光...",
+                       },
+                     ],
                    },
-                   {
-                     :japanese => "龗神社",
-                     :english => "Okami-jinja Shrine"
-                   },
-                  ],
-                  [
-                    records.size,
-                    records[0].to_h,
-                    records[-1].to_h,
-                  ])
+                   hashify(first_record))
+    end
+  end
+
+  sub_test_case("lexicon") do
+    def setup
+      @dataset = Datasets::WikipediaKyotoJapaneseEnglish.new(type: :lexicon)
+    end
+
+    test("#each") do
+      records = @dataset.each.to_a
+      assert_equal([
+                     51982,
+                     {
+                       :japanese => "102世吉田日厚貫首",
+                       :english => "the 102nd head priest, Nikko TOSHIDA"
+                     },
+                     {
+                       :japanese => "龗神社",
+                       :english => "Okami-jinja Shrine"
+                     },
+                    ],
+                    [
+                      records.size,
+                      records[0].to_h,
+                      records[-1].to_h,
+                    ])
+    end
   end
 
   test("invalid") do
@@ -81,7 +163,8 @@ class WikipediaKyotoJapaneseEnglishTest < Test::Unit::TestCase
   end
 
   test("description") do
-    description = @dataset_articles.metadata.description
+    dataset = Datasets::WikipediaKyotoJapaneseEnglish.new
+    description = dataset.metadata.description
     assert_equal(<<-DESCRIPTION, description)
 "The Japanese-English Bilingual Corpus of Wikipedia's Kyoto Articles"
 aims mainly at supporting research and development relevant to

72330bd9265fceb17e841d32361d3807455d0579
Sutou Kouhei <kou@cozmixng.org>
2022-08-28 22:11:06 +0900

    Use Rubyish API

--- example/quora-duplicate-question-pair.rb
+++ example/quora-duplicate-question-pair.rb
@@ -10,7 +10,7 @@ question_pair.each do |pair|
     pair.second_question_id,
     pair.first_question,
     pair.second_question,
-    pair.duplicated
+    pair.duplicated?
   ]
   # [0, 1, 2, "What is the step by step guide to invest in share market in india?", "What is the step by step guide to invest in share market?", false]
   # [1, 3, 4, "What is the story of Kohinoor (Koh-i-Noor) Diamond?", "What would happen if the Indian government stole the Kohinoor (Koh-i-Noor) diamond back?", false]

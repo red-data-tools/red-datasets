d210c3f05d411f1331aa27b2a18110030ff739bc
Tsutomu Katsube <tsuto.katsube@gmail.com>
2024-01-21 07:26:25 +0900

    Use #each
    Because this is a test for #each
    
    Co-authored-by: Sutou Kouhei <kou@cozmixng.org>

--- test/test-quora-duplicate-question-pair.rb
+++ test/test-quora-duplicate-question-pair.rb
@@ -8,13 +8,12 @@ class QuoraDuplicateQuestionPairTest < Test::Unit::TestCase
   end
 
   test("#each") do
-    records = @dataset
     assert_equal(record(0,
                         1,
                         2,
                         "What is the step by step guide to invest in share market in india?",
                         "What is the step by step guide to invest in share market?",
                         false),
-                 records.first)
+                 @dataset.each.next)
   end
 end

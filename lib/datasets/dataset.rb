714a405c5678d2fc1f1b0868d8b58a86803c0091
Tsutomu Katsube <tsuto.katsube@gmail.com>
2024-07-03 05:45:51 +0900

    Remove < and > from XXX_url
    Because `<` and `>` aren't part of the URL.
    
    Co-authored-by: Sutou Kouhei <kou@clear-code.com>

--- lib/datasets/dataset.rb
+++ lib/datasets/dataset.rb
@@ -41,10 +41,10 @@ module Datasets
         return
       rescue Net::HTTPClientException => error
         if urls.last != url
-          failed_url = "<#{url}>"
-          fallback_url = "<#{urls[index + 1]}>"
+          failed_url = url
+          fallback_url = urls[index + 1]
           message = "#{error.response.code} #{error.response.message}: " +
-                    "fallback: #{failed_url} -> #{fallback_url}"
+                    "fallback: <#{failed_url}> -> <#{fallback_url}>"
           $stderr.puts(message)
         else
           raise error

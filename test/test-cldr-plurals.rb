06516fca1feb41ef9bef78053c98942d22f97690
Sutou Kouhei <kou@clear-code.com>
2025-06-09 05:59:03 +0900

    test cldr-plurals: update expected

--- test/test-cldr-plurals.rb
+++ test/test-cldr-plurals.rb
@@ -14,7 +14,7 @@ class CLDRPluralsTest < Test::Unit::TestCase
   test("#each") do
     locales = @dataset.each.to_a
     assert_equal([
-                   222,
+                   225,
                    locale("bm",
                           [
                             rule("other",
